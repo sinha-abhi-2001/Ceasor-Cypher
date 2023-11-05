397ec81461f4d8b243d4869658bbac10ebdd324e
Abhishek Sinha <sinha00abhishek@gmail.com>
2023-11-05 09:50:20 +0530

    added encrypt function and enhanced UI

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #CAESAR CIPHER PROJECT
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#required variables declaration
+direction = input("type encode to encrypt or type decode to decrypt")
+text = input("Enter the text: ")
+shift = int(input("Enter the shift amount"))
+
