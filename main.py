5d72171ed4fa4b50c1221cf8155b17580e4f3706
Abhishek Sinha <sinha00abhishek@gmail.com>
2023-11-11 23:24:05 +0530

    empty commit

--- main.py
+++ main.py
@@ -7,29 +7,19 @@ direction = input("type encode to encrypt or type decode to decrypt\n")
 text = input("Enter the text: ")
 shift = int(input("Enter the shift amount: "))
 
-
-def encrypt(plain_text, shift_amount):
-    cipher_text=""
-    for letter in plain_text:
+def caesar(input_text,shift_amount,user_direction):
+    output_text=""
+    for letter in input_text:
         position = alphabet.index(letter)
-        new_position = position + shift_amount
-        cipher_text += alphabet[new_position]
-    print(f"The decoded text is {cipher_text}")
+        if direction == "encode":
+            new_position = position + shift_amount
+            output_text += alphabet[new_position]
+        elif direction == "decode":
+            new_position = position - shift_amount
+            output_text += alphabet[new_position]
+        else:
+            print("Wrong input")
+            break
+    print(f"Your desired answer is {output_text}")
 
-# encrypt(plain_text=text, shift_amount=shift)
-
-def decrypt(cipher_text, shift_amount):
-    plain_text=""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"The encoded text is {plain_text}")
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
-else:
-    print("Wrong Input")
-def caesar()
+caesar(input_text=text,shift_amount=shift,user_direction=direction)
