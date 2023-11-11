5305dd02a769ac76b97fd1a12f39dec2c4774718
Abhishek Sinha <sinha00abhishek@gmail.com>
2023-11-11 21:44:38 +0530

    added encrypt function

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 #required variables declaration
-direction = input("type encode to encrypt or type decode to decrypt")
+direction = input("type encode to encrypt or type decode to decrypt\n")
 text = input("Enter the text: ")
-shift = int(input("Enter the shift amount"))
+shift = int(input("Enter the shift amount: "))
 
+def encrypt(plain_text, shift_amount):
+    cipher_text=""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
