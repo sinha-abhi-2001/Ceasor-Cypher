3eaf1612de8b3ef7efa51c986fb0ba4c8b4db2bc
Abhishek Sinha <sinha00abhishek@gmail.com>
2023-11-11 22:46:50 +0530

    added decrypt function and user input option

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ direction = input("type encode to encrypt or type decode to decrypt\n")
 text = input("Enter the text: ")
 shift = int(input("Enter the shift amount: "))
 
+
 def encrypt(plain_text, shift_amount):
     cipher_text=""
     for letter in plain_text:
@@ -15,4 +16,19 @@ def encrypt(plain_text, shift_amount):
         cipher_text += alphabet[new_position]
     print(f"The encoded text is {cipher_text}")
 
-encrypt(plain_text=text, shift_amount=shift)
+# encrypt(plain_text=text, shift_amount=shift)
+
+def decrypt(cipher_text, shift_amount):
+    plain_text=""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The encoded text is {plain_text}")
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
+else:
+    print("Wrong Input")
